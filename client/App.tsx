d9974dfde27cc8054bb9cf59cfc42a93b8226368
Eugene Malin(Novikov) <jenia96@live.com>
2019-12-19 06:59:28 +0300

    Add client part using expo cli

---
+++ client/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Book Me!</Text>
+      <Text>Вот так начинаются приложения!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
