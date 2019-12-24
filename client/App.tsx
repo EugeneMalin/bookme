02b76d0c4d14cd9121225e1669b317d886edcebd
Eugene Malin(Novikov) <jenia96@live.com>
2019-12-24 07:25:10 +0300

    Add default client mockup

--- client/App.tsx
+++ client/App.tsx
@@ -1,20 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Book Me!</Text>
-      <Text>Вот так начинаются приложения!</Text>
-    </View>
-  );
-}
+import Navigator from './src/navigations';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => <Navigator />;
+
+export default App;
