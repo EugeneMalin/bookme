8724e0a6e7835c7d8b912e8ff11670dbf490d2eb
Eugene Malin(Novikov) <jenia96@live.com>
2020-01-02 20:54:09 +0300

    Add sign in and sign up events

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,8 +2,11 @@ import dotenv from "dotenv";
 import express from "express";
 import { createServer } from "http";
 import path from "path";
+import {Op} from "sequelize";
 import socketIO from "socket.io";
 import logger from "./lib/log";
+import { connection } from "./lib/sequelize";
+import User, { IUser } from "./model/user";
 
 // initialize configuration
 dotenv.config();
@@ -19,8 +22,59 @@ const io = socketIO(http);
 app.set( "views", path.join( __dirname, "views" ) );
 app.set( "view engine", "ejs" );
 
-io.on("connection", (socket) => {
-    logger.info("connected!");
+connection.sync().then(() => {
+    logger.info("database synced!");
+
+    io.on("connection", (socket) => {
+        logger.info("person connected!");
+        socket.on("signUp", ({username, email, password}: IUser) => {
+            const res = User.get({email, username, password}).save();
+            res.then((user) => {
+                if (user) {
+                    socket.emit("signedUp", {
+                        username
+                    });
+                }
+            });
+            res.error((reason) => {
+                socket.emit("error", {
+                    error: reason,
+                    event: "signup",
+                });
+            });
+            res.catch((error) => {
+                socket.emit("error", {
+                    error,
+                    event: "signup",
+                });
+            });
+        });
+        socket.on("signIn", ({username, email, password}: IUser) => {
+            User.findOne({
+                where: {
+                    [Op.or]: [{username}, {email}]
+                }
+            }).then((user) => {
+                if (user) {
+                    if (user.check(password)) {
+                        socket.emit("signedIn", {
+                            createdAt: user.createdAt,
+                            email: user.email,
+                            username: user.username,
+                        });
+                    }
+                    socket.emit("error", {
+                        error: new Error(`Password ${email || username} isn't right`),
+                        event: "signin",
+                    });
+                }
+                socket.emit("error", {
+                    error: new Error(`No user ${email || username}`),
+                    event: "signin",
+                });
+            });
+        });
+    });
 });
 
 // define a route handler for the default home page
