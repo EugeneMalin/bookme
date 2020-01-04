580b6d28a39d96cd793c06cb19782b7e17256804
Eugene Malin(Novikov) <jenia96@live.com>
2020-01-04 10:16:14 +0300

    Add scroll view

--- client/src/components/Background.tsx
+++ client/src/components/Background.tsx
@@ -3,22 +3,29 @@ import {
   ImageBackground,
   StyleSheet,
   KeyboardAvoidingView,
+  SafeAreaView,
+  ScrollView
 } from 'react-native';
+import Constants from 'expo-constants';
 
 type Props = {
   children: React.ReactNode;
 };
 
 const Background = ({ children }: Props) => (
-  <ImageBackground
-    source={require('../assets/background_dot.png')}
-    resizeMode="repeat"
-    style={styles.background}
-  >
-    <KeyboardAvoidingView style={styles.container} behavior="padding">
-      {children}
-    </KeyboardAvoidingView>
-  </ImageBackground>
+  <SafeAreaView style={styles.scrollContainer}>
+    <ScrollView style={styles.scrollView}>
+      <ImageBackground
+      source={require('../assets/background_dot.png')}
+      resizeMode="repeat"
+      style={styles.background}
+    >
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        {children}
+      </KeyboardAvoidingView>
+    </ImageBackground>
+    </ScrollView>
+  </SafeAreaView>
 );
 
 const styles = StyleSheet.create({
@@ -34,6 +41,13 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  scrollContainer: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+  },
+  scrollView: {
+    marginHorizontal: 20,
   },
 });
 
