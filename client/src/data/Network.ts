e95d8fc7f44e400dd81447e8ee5ff1d122e1c0b0
eugene.malin <jenia96@live.com>
2020-12-18 12:18:31 +0300

    Add data helpers

---
+++ client/src/data/Network.ts
@@ -0,0 +1,15 @@
+/**
+ * todo realease me
+ * @param login string with login
+ */
+export function isUniqueLogin(login: string): boolean {
+    return login != 'unique';
+}
+
+/**
+ * todo realease me
+ * @param email string with email address
+ */
+export function isUniqueEmail(email: string): boolean {
+    return email != 'unique';
+}
---
+++ client/src/data/User.ts
@@ -0,0 +1,24 @@
+import { IUser } from "./interface/IUser";
+
+/**
+ * User data class
+ */
+export class User implements IUser {
+    login: string;
+    email: string;
+    password: string;
+
+    name: string;
+    surname: string;
+    patronymic: string;
+
+    constructor(params: IUser) {
+        this.login = params.login;
+        this.password = params.password;
+        this.email = params.email;
+
+        this.name = params.name;
+        this.surname = params.surname || '';
+        this.patronymic = params.patronymic || '';
+    }
+}
