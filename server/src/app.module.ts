432db38cd801d52dc44fb1acb9c391da19175d11
eugene.malin <jenia96@live.com>
2021-01-05 12:36:00 +0300

    Correct database config

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+require('dotenv').config()
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    entities: ["dist/**/*.entity{.ts,.js}"],
+    synchronize: !!process.env.DEBUG,
+  })],
   controllers: [],
 })
 export class AppModule {}
