f51d152d83b03b872c1136a8fa66aa6997722bc1
eugene.malin <jenia96@live.com>
2021-01-09 02:22:58 +0300

    Add thunk function

--- client/src/data/Network.ts
+++ client/src/data/Network.ts
@@ -1,17 +1,40 @@
 import 'isomorphic-fetch';
+import { IUser } from './interface/IUser';
+const REMOVE_SERVER_TYPE = 'http'
+const REMOTE_SERVER_HOST = 'localhost'
+const REMOTE_SERVER_PORT = 3000
 
-/**
- * todo realease me
- * @param login string with login
- */
-export function isUniqueLogin(login: string): boolean {
-    return login != 'unique';
+type ErrorRespose = {
+    code: number,
+    message: string
+} 
+
+export function fetchThenDispatch<K, T>(
+    url: string,
+    method: string,
+
+    body?: K,
+    headers?: any,
+
+    dispatch: (item: T) => T = res => res
+): Promise<T|ErrorRespose> {
+    return fetch(
+        `${REMOVE_SERVER_TYPE}://${REMOTE_SERVER_HOST}:${REMOTE_SERVER_PORT}/${url}`, {
+            method,
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': `${REMOVE_SERVER_TYPE}://${REMOTE_SERVER_HOST}:${REMOTE_SERVER_PORT}`,
+                ...headers
+            }
+        }
+    )
+        .then(resp => resp.json())
+        .then(dispatch)
+        .catch(err => ({code: -1, message: err}))
 }
 
-/**
- * todo realease me
- * @param email string with email address
- */
-export function isUniqueEmail(email: string): boolean {
-    return email != 'unique';
+
+export function createUser(userDto: IUser): Promise<void|ErrorRespose> {
+    return fetchThenDispatch<IUser, void|ErrorRespose>('user', 'POST', userDto);
 }
