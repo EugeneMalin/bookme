53da71438e581391dc59b62717a9a6c2caf3f8d6
eugene.malin <jenia96@live.com>
2021-01-17 03:12:25 +0300

    Correct env and full user test

--- server/src/user/user.spec.ts
+++ server/src/user/user.spec.ts
@@ -4,4 +4,11 @@ describe('User', () => {
   it('should be defined', () => {
     expect(new User()).toBeDefined();
   });
+
+  describe('hashPassword', () => {
+    it('should hash same password for same string', () => {
+      const pass = '12345678'
+      expect(User.hashPassword(pass)).toBe(User.hashPassword(pass));
+    })
+  })
 });
