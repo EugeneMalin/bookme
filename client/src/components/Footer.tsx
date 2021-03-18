c74b0dd64a6f250bd302b8cdb8c710d19af677b3
eugene.malin <jenia96@live.com>
2021-03-18 22:01:01 +0300

    Correct footer for redux

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,8 +1,12 @@
 import { BottomNavigation, BottomNavigationAction, makeStyles } from "@material-ui/core";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent } from "react";
 import { IBase } from "./Base"
+import HomeIcon from '@material-ui/icons/Home';
 import PeopleIcon from '@material-ui/icons/People';
 import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
+import { useDispatch, useSelector } from "react-redux";
+import { IStore } from "../store";
+import actions from "../actions";
 
 export interface IFooter extends IBase {
 
@@ -16,13 +20,15 @@ const useStyles = makeStyles({
 
 export const Footer = (props: IFooter) => {
   const classes = useStyles();
-  const [value, setValue] = useState('people');
+  const value = useSelector<IStore>(store => store.tabId);
+  const dispatch = useDispatch();
   const handleChange = (event: ChangeEvent<{}>, newValue: string) => {
-    setValue(newValue);
+    dispatch(actions.tab.change(newValue));
   };
 
   return <footer className={props.className}>
         <BottomNavigation value={value} onChange={handleChange} className={classes.root}>
+            <BottomNavigationAction label="Home" value="home" icon={<HomeIcon />} />
             <BottomNavigationAction label="People" value="people" icon={<PeopleIcon />} />
             <BottomNavigationAction label="Books" value="books" icon={<LibraryBooksIcon />} />
         </BottomNavigation>
